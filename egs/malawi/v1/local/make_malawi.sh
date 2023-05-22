e6baa916a2df20a67010e3744f130029bee329ce
jessiewang-hongyan <jessiewang529@gmail.com>
2023-05-22 13:40:31 -0400

    add make_malawi.sh

--- egs/malawi/v1/local/make_malawi.sh
+++ egs/malawi/v1/local/make_malawi.sh
@@ -4,27 +4,30 @@
 #
 # Creates the DIHARD 2019 data directories.
 
-if [ $# != 2 ]; then
-  echo "Usage: $0 <dihard-dir> <data-dir>"
-  echo " e.g.: $0 /export/corpora/LDC/LDC2019E31 data/dihard2019_dev"
-fi
+#if [ $# != 2 ]; then
+#  echo "Usage: $0 <dihard-dir> <data-dir>"
+#  echo " e.g.: $0 /export/corpora/LDC/LDC2019E31 data/dihard2019_dev"
+#fi
 
-dihard_dir=$1/data/single_channel
-data_dir=$2
+#dihard_dir=$1/data/single_channel
+#data_dir=$2
+
+malawi_dir=/export/fs05/leibny/CCWD-Fe62023/langdev
+data_dir=/export/fs05/ywang793/malawi_data
 
 echo "making data dir $data_dir"
 
 mkdir -p $data_dir
 
-find $dihard_dir -name "*.flac" | \
+find $malawi_dir -name "*.mp3" | \
     awk '
-{ bn=$1; sub(/.*\//,"",bn); sub(/\.flac$/,"",bn); 
-  print bn, "sox "$1" -t wav -b 16 -e signed-integer - |" }' | sort -k1,1 > $data_dir/wav.scp
+{ bn=$1; sub(/.*\//,"",bn); sub(/\.mp3$/,"",bn);
+  print bn, "ffmpeg -i "$1" "$1".wav - |" }' | sort -k1,1 > $data_dir/wav.scp
 
 awk '{ print $1,$1}' $data_dir/wav.scp  > $data_dir/utt2spk
 cat $data_dir/utt2spk > $data_dir/spk2utt
 
-for f in $(find $dihard_dir -name "*.lab" | sort)
+for f in $(find $malawi_dir -name "*.lab" | sort)
 do
     awk '{ bn=FILENAME; sub(/.*\//,"",bn); sub(/\.lab$/,"",bn); 
            printf "%s-%010d-%010d %s %f %f\n", bn, $1*1000, $2*1000, bn, $1, $2}' $f
@@ -32,14 +35,14 @@ done > $data_dir/vad.segments
 
 
 rm -f $data_dir/reco2num_spks
-for f in $(find $dihard_dir -name "*.rttm" | sort)
+for f in $(find $malawi_dir -name "*.rttm" | sort)
 do
     cat $f
     awk '{ print $2, $8}' $f | sort -u | awk '{ f=$1; count++}END{ print f, count}' >> $data_dir/reco2num_spks
 
 done > $data_dir/diarization.rttm
 
-for f in $(find $dihard_dir -name "*.uem" | sort)
+for f in $(find $malawi_dir -name "*.uem" | sort)
 do
     cat $f
 done > $data_dir/diarization.uem
--- egs/malawi/v1/run_001_prepare_data.sh
+++ egs/malawi/v1/run_001_prepare_data.sh
@@ -13,10 +13,10 @@
 # Submit to GPU
 #$ -q g.q
 
-log_file='/export/c12/ywang793/logs/log.dihard2019_prepare'
-echo "------------
-working directory: $(pwd)
----------------" >> "$log_file"
+#log_file='/export/c12/ywang793/logs/log.dihard2019_prepare'
+#echo "------------
+#working directory: $(pwd)
+#---------------" >> "$log_file"
 
 
 . ./cmd.sh
@@ -29,19 +29,19 @@ config_file=default_config.sh
 . $HYP_ROOT/hyp_utils/parse_options.sh || exit 1;
 . datapath.sh 
 
-if [ $stage -le 1 ];then
-
-    # Prepare the VoxCeleb1 dataset for training.
-    local/make_voxceleb1cat.pl $voxceleb1_root 16 data
-
-    # Prepare the VoxCeleb2 dataset for training.
-    local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
-    utils/combine_data.sh data/voxcelebcat data/voxceleb1cat data/voxceleb2cat_train
-fi
+#if [ $stage -le 1 ];then
+#
+#    # Prepare the VoxCeleb1 dataset for training.
+#    local/make_voxceleb1cat.pl $voxceleb1_root 16 data
+#
+#    # Prepare the VoxCeleb2 dataset for training.
+#    local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
+#    utils/combine_data.sh data/voxcelebcat data/voxceleb1cat data/voxceleb2cat_train
+#fi
 
 if [ $stage -le 2 ];then
     # prepare Dihard2019
 #    local/make_dihard2019.sh $dihard2019_dev data/dihard2019_dev
 #    local/make_dihard2019.sh $dihard2019_eval data/dihard2019_eval
-    local/make_dihard2019.sh
+    local/make_malawi.sh
 fi
