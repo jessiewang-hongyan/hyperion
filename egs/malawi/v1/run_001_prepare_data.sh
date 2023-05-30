b8c0363b6958c197d6b97bf9b98f93e1bf6b8737
jessiewang-hongyan <jessiewang529@gmail.com>
2023-05-30 09:35:29 -0400

    prepare utt2spk by filename

--- egs/malawi/v1/run_001_prepare_data.sh
+++ egs/malawi/v1/run_001_prepare_data.sh
@@ -21,18 +21,19 @@ config_file=default_config.sh
 . parse_options.sh || exit 1;
 . datapath.sh 
 
-if [ $stage -le 1 ];then
+# if [ $stage -le 1 ];then
 
-    # Prepare the VoxCeleb1 dataset for training.
-    local/make_voxceleb1cat.pl $voxceleb1_root 16 data
+#     # Prepare the VoxCeleb1 dataset for training.
+#     local/make_voxceleb1cat.pl $voxceleb1_root 16 data
 
-    # Prepare the VoxCeleb2 dataset for training.
-    local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
-    utils/combine_data.sh data/voxcelebcat data/voxceleb1cat data/voxceleb2cat_train
-fi
+#     # Prepare the VoxCeleb2 dataset for training.
+#     local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
+#     utils/combine_data.sh data/voxcelebcat data/voxceleb1cat data/voxceleb2cat_train
+# fi
 
-if [ $stage -le 2 ];then
-    # prepare Dihard2019
-    local/make_dihard2019.sh $dihard2019_dev data/dihard2019_dev
-    local/make_dihard2019.sh $dihard2019_eval data/dihard2019_eval
-fi
+# if [ $stage -le 2 ];then
+#     # prepare Dihard2019
+#     local/make_dihard2019.sh $dihard2019_dev data/dihard2019_dev
+#     local/make_dihard2019.sh $dihard2019_eval data/dihard2019_eval
+# fi
+# local/make_dihard2019.sh  [where data in] [information/output of data]
