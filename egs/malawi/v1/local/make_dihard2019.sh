ba36912d9f66c44ab57c72211ea936ed7a2007f0
jessiewang-hongyan <jessiewang529@gmail.com>
2023-06-03 16:01:01 -0400

    debug: wav.scp debug

--- egs/malawi/v1/local/make_dihard2019.sh
+++ egs/malawi/v1/local/make_dihard2019.sh
@@ -35,7 +35,7 @@ find $dihard_dir -name "*.mp3" | \
     awk '
 { bn=$1; sub(/.*\//,"",bn); sub(/\.mp3$/,"",bn);
   split(bn, parts, "_");
-  printf "%s_%s ffmpeg -i %s %s.wav - |", parts[1], parts[2], $1, $1 }' | sort -k1,1 > $data_dir/wav.scp
+  printf "ffmpeg -i %s %s.wav - |", $1, $1 }' | sort -k1,1 > $data_dir/wav.scp
 
 awk '{bn=$1; sub(/.*\//,"",bn); sub(/\.mp3$/,"",bn);
       split(bn, parts, "_");
