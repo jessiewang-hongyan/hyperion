d8a0c21e3d9cd10a37f457624c5ba1bf289a69f9
jessiewang-hongyan <jessiewang529@gmail.com>
2023-06-27 17:40:51 -0400

    find: missing argument for '-name'

--- egs/pho-lid/v1/data_prepare.py
+++ egs/pho-lid/v1/data_prepare.py
@@ -67,11 +67,12 @@ class label_reader(object):
                 temp['utt'] = utt
                 temp['length'] = length
                 
-                if audio not in self.audio_segs.keys() and audio != 'audio_name': #'TTS_P10040TT_VCST_ECxxx_01_AO_35259847_v001_R004_CRR_MERLIon-CCS.wav':
-                    self.audio_segs[audio] = list()
-                
-                #if audio == 'TTS_P10040TT_VCST_ECxxx_01_AO_35259847_v001_R004_CRR_MERLIon-CCS.wav':
-                self.audio_segs[audio].append(temp)
+                if audio != 'audio_name':
+                    if audio not in self.audio_segs.keys() and audio != 'audio_name': #'TTS_P10040TT_VCST_ECxxx_01_AO_35259847_v001_R004_CRR_MERLIon-CCS.wav':
+                        self.audio_segs[audio] = list()
+                    
+                    #if audio == 'TTS_P10040TT_VCST_ECxxx_01_AO_35259847_v001_R004_CRR_MERLIon-CCS.wav':
+                    self.audio_segs[audio].append(temp)
                            
     def read_audio_seg(self):
         # mapping file
