f57f4c833727a0b63ec55da02b96c75ccad5de94
jessiewang-hongyan <jessiewang529@gmail.com>
2023-06-27 09:36:57 -0400

    convert speech

--- egs/pho-lid/v1/data_prepare.py
+++ egs/pho-lid/v1/data_prepare.py
@@ -21,16 +21,23 @@ from transformers import Wav2Vec2Processor, Wav2Vec2FeatureExtractor, Wav2Vec2Mo
 class feature_extract(object):
     def __init__(self, save_path):
         super().__init__()
-        self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-large-xlsr-53")
-        self.processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
-        self.save_path = save_path
+        model_name = "facebook/wav2vec2-large-xlsr-53"
+        self.processor = Wav2Vec2Processor.from_pretrained(model_name)
+        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
 
     def read_wav(self, filepath:str):
         waveform, sample_rate = torchaudio.load(filepath)
         inputs = self.processor(waveform, sampling_rate=sample_rate, return_tensors="pt", padding=True)
 
+        # Resample if necessary
+        xlsr_rate = self.processor.feature_extractor.sampling_rate
+        if sample_rate != xlsr_rate:
+            waveform = torchaudio.transforms.Resample(sample_rate, xlsr_rate)(waveform)
+        # extract features
+        input_values = self.feature_extractor(waveform, sampling_rate=xlsr_rate, return_tensors="pt").input_values
+        
         with torch.no_grad():
-            features = self.model.extract_features(inputs.input_values)
+            features = self.processor.model(input_values).last_hidden_state
             array = features.numpy()
             np.save(self.save_path+filepath.replace('.wav', '')+'.npy', array)
 
