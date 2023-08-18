d9cd5fd1c716a1c7023b0e901efa0f0cd60df7b6
jessiewang-hongyan <jessiewang529@gmail.com>
2023-08-18 12:23:19 -0400

    merge seame

--- egs/pho-lid/v1/run_001_prepare_data.sh
+++ egs/pho-lid/v1/run_001_prepare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#$ -N seame_pure2vec
-#$ -j y -o /export/c12/ywang793/logs/log.seame_pure2vec
+#$ -N seame_merge
+#$ -j y -o /export/c12/ywang793/logs/log.seame_merge
 #$ -M ywang793@jh.edu
 #$ -m e
 #$ -l ram_free=20G,mem_free=20G,gpu=1,hostname=d01
